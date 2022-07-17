7eca688582b89031c317401f39b32f2efafaa1bd
ats3md <ats3md@umsystem.edu>
2022-07-17 14:22:08 -0500

    Added option to start new game completely if needed without restarting program.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 import game.Game;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // FIXME: 7/17/2022 add outer game loop
-        Game game = new Game();
-        game.startGame();
+        boolean playingGame = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (playingGame) {
+            System.out.println("Starting hydra slaying game.");
+            Game game = new Game();
+            game.startGame();
+            System.out.print("Press Q to quit. Press anything else to play again: ");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("Q") || userInput.equals("q")) {
+                playingGame = false;
+            }
+
+        }
+        System.out.println("\nEnding hydra slaying game. ");
     }
 }
